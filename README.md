c028143fd023beecc3e83ec10b6ce3c369986e56
Le Soleil <mra4naja@list.ru>
2024-02-28 19:38:07 +0300

    добавление игры Проверка на четность

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Lee-Soleil/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Lee-Soleil/python-project-49/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/4d94a90f01e91e1148b3/maintainability)](https://codeclimate.com/github/Lee-Soleil/python-project-49/maintainability)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import prompt
+from random import randint
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i < 3:
+        random_number = randint(1, 100)
+        print('Question: ' + str(random_number))
+        answer = prompt.string('Your answer: ')
+        if random_number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        if (random_number % 2 == 0 and answer == 'yes') or (random_number % 2 !=0 and answer == 'no'):
+            print('Correct!')
+            if i == 2:
+                print("Congratulations, " + name + "!")
+        else:
+            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + correct_answer + "'.")
+            print("Let's try again, " + name + "!")
+            break
+        i += 1
+
+
+
+if __name__ == '__main__':
+    main()
