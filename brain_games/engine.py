bc86bcc16e2f9d205c51fb65cb0b25c02649cc8f
Le Soleil <mra4naja@list.ru>
2024-03-03 21:05:41 +0300

    исправление амечаний CodeClimat

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,6 +1,9 @@
 import prompt
 
 
+COUNT_ROUND = 1
+
+
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
@@ -11,19 +14,19 @@ def welcome_user():
 def play(game):
     name = welcome_user()
     print(game.rules_of_the_game)
-    count_round = 0
-    while count_round < 3:
+    COUNT_ROUND = 1
+    while COUNT_ROUND <= 3:
         question = game.get_question()
         correct_answer = game.get_correct_answer()
         print('Question: ' + str(question))
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
-            if count_round == 2:
-                print("Congratulations, " + name + "!")
         else:
             print(f"'{answer}' is wrong answer ;(. "
                   f"Correct answer was '{str(correct_answer)}'")
             print("Let's try again, " + name + "!")
             break
-        count_round += 1
+        if COUNT_ROUND == 3 and answer == correct_answer:
+            print("Congratulations, " + name + "!")
+        COUNT_ROUND += 1
